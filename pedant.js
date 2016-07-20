8598752ca42f0685be5a56c1bc4b311492ced187
Decagon <Decagon@users.noreply.github.com>
2016-07-19 22:06:47 -0300

    Initial commit; primitive punctuation checking

--- pedant.js
+++ pedant.js
@@ -1,1 +1,12 @@
+var text = "hello,this is a bad sentence.";
 
+
+var punctuation = ",.!;";
+
+for (var i = 0; i < text.length; i++) {
+  if (punctuation.indexOf(text[i]) > -1) {
+    if ((text[i+1] != " ") && (i != text.length-1)) {
+      console.log("Pedant: punctuation error at index " + (i+1) + ": " + text);
+    }
+  }
+}
